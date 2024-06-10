00c400e3b1bed960462fedf29f13db49b3927140
dharmateja2008 <dharmateja.208@gmail.com>
2024-06-09 23:35:52 -0400

    Add files via upload

---
+++ CodeTest.Tests/PhoneNumberValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CodeTest.Tests
+{
+    public class PhoneNumberValidatorTests
+    {
+        [Theory]
+        [InlineData("", false)]
+        [InlineData("555555", false)]
+        [InlineData("5555555", true)]
+        [InlineData("55*5555", false)]
+        [InlineData("55#5555", false)]
+        [InlineData("1555555", false)]
+        [InlineData("0555555", false)]
+        public void IsValid_ReturnsCorrectValue(string phoneNumber, bool expectedResult)
+        {
+            var sut = new PhoneNumberValidator();
+            var isValid = sut.IsValid(phoneNumber);
+            Assert.Equal(expectedResult, isValid);            
+        }
+    }
+}
